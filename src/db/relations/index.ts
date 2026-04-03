26b704eac0d6f2cb225052a63ad399cac58b5632
Ade Chandra1 <adechondra@gmail.com>
2026-04-03 14:50:23 +0700

    forgot to rename entities in index db relation

--- src/db/relations/index.ts
+++ src/db/relations/index.ts
@@ -1,53 +1,53 @@
 import { relations } from "drizzle-orm";
-import { user } from "../entities/user";
-import { session } from "../entities/session";
-import { account } from "../entities/account";
-import { invitation, member, organization } from "../entities/organization";
+import { users } from "../entities/user";
+import { sessions } from "../entities/session";
+import { accounts } from "../entities/account";
+import { invitations, members, organizations } from "../entities/organization";
 
-export const userRelations = relations(user, ({ many }) => ({
-  sessions: many(session),
-  accounts: many(account),
-  members: many(member),
-  invitations: many(invitation),
+export const usersRelations = relations(users, ({ many }) => ({
+  sessionss: many(sessions),
+  accountsss: many(accounts),
+  memberss: many(members),
+  invitationss: many(invitations),
 }));
 
-export const sessionRelations = relations(session, ({ one }) => ({
-  user: one(user, {
-    fields: [session.userId],
-    references: [user.id],
+export const sessionsRelations = relations(sessions, ({ one }) => ({
+  users: one(users, {
+    fields: [sessions.userId],
+    references: [users.id],
   }),
 }));
 
-export const accountRelations = relations(account, ({ one }) => ({
-  user: one(user, {
-    fields: [account.userId],
-    references: [user.id],
+export const accountssRelations = relations(accounts, ({ one }) => ({
+  users: one(users, {
+    fields: [accounts.userId],
+    references: [users.id],
   }),
 }));
 
-export const organizationRelations = relations(organization, ({ many }) => ({
-  members: many(member),
-  invitations: many(invitation),
+export const organizationsRelations = relations(organizations, ({ many }) => ({
+  memberss: many(members),
+  invitationss: many(invitations),
 }));
 
-export const memberRelations = relations(member, ({ one }) => ({
-  organization: one(organization, {
-    fields: [member.organizationId],
-    references: [organization.id],
+export const membersRelations = relations(members, ({ one }) => ({
+  organizations: one(organizations, {
+    fields: [members.organizationId],
+    references: [organizations.id],
   }),
-  user: one(user, {
-    fields: [member.userId],
-    references: [user.id],
+  users: one(users, {
+    fields: [members.userId],
+    references: [users.id],
   }),
 }));
 
-export const invitationRelations = relations(invitation, ({ one }) => ({
-  organization: one(organization, {
-    fields: [invitation.organizationId],
-    references: [organization.id],
+export const invitationsRelations = relations(invitations, ({ one }) => ({
+  organizations: one(organizations, {
+    fields: [invitations.organizationId],
+    references: [organizations.id],
   }),
-  user: one(user, {
-    fields: [invitation.inviterId],
-    references: [user.id],
+  users: one(users, {
+    fields: [invitations.inviterId],
+    references: [users.id],
   }),
 }));
